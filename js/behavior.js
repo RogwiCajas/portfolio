026133e69935f63ebc58ebc7c29073cba265381f
RogwiCajas <rogwinalex2@hotmail.com>
2020-11-13 19:15:23 -0500

    portfolio v1 listo para deploy

---
+++ js/behavior.js
@@ -0,0 +1,15 @@
+
+function mostrarMenu(){
+    //obtener estdo del display, si es none cambiarlo a flex sino cambiarlo a none
+    
+    if(document.getElementById("op").style.display =='block' ){
+        document.getElementById("op").style.display = 'none';
+    }else{
+        document.getElementById("op").style.display = 'block';
+        
+    }
+
+    
+}
+
+
